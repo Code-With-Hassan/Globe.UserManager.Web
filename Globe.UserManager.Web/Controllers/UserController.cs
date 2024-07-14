2a96993bec8d0b341baa510344520bdb72b8d708
Code-With-Hassan <muhammadhassanshafique5@GMAIL.COM>
2024-07-14 17:08:06 +0500

    Create users screen (FE)

---
+++ Globe.UserManager.Web/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Globe.UserManager.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
